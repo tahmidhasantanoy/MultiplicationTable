e97620cdf4f7312593abd0fe2f0e113ddc7cc87d
Tahmid Hasan <tahmid.hasan.tanoy@gmail.com>
2019-05-24 12:14:42 -0700

    fixed heading

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
     printf("Number : ");
     scanf("%d", &n);
     
-    printf("Multiplication table of %n:\n", n);
+    printf("Multiplication table of %d : \n", n);
 
     for (int i = 1; i <= 10; i++) {
         printf("%d * %d = %d \n", n, i, (n * i));
