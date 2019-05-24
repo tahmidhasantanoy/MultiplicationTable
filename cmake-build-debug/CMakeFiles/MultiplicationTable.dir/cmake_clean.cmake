1c74f79dedd3144d00755ee2c01cd943f2176b75
Tahmid Hasan <tahmid.hasan.tanoy@gmail.com>
2019-05-24 12:02:32 -0700

    implemented multiplication table

---
+++ cmake-build-debug/CMakeFiles/MultiplicationTable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultiplicationTable.dir/main.c.obj"
+  "MultiplicationTable.pdb"
+  "MultiplicationTable.exe"
+  "MultiplicationTable.exe.manifest"
+  "libMultiplicationTable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MultiplicationTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
