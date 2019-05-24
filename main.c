6a8f3287f130cfadd80ec66bd017f569eaaa3135
Tahmid Hasan <50996889+tahmidhasantanoy@users.noreply.github.com>
2019-05-24 12:12:39 -0700

    added heading

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main() {
 
     printf("Number : ");
     scanf("%d", &n);
+    
+    printf("Multiplication table of %n:\n", n);
 
     for (int i = 1; i <= 10; i++) {
         printf("%d * %d = %d \n", n, i, (n * i));
